6a2c5c864e58b55bbdbc7384e1a245b83b218f92
Hexcaton <noucprs@gmail.com>
2023-11-20 06:26:56 +0600

    Added hw3

---
+++ HW/hw3/2.py
@@ -0,0 +1,7 @@
+print('Введите целое число через Enter:')
+int_a = int(input())
+
+if int_a % 2:
+    print(False)
+else:
+    print(True)
---
+++ HW/hw3/3.py
@@ -0,0 +1,10 @@
+print('Введите три целых числа через Enter:')
+
+int_a = int(input())
+int_b = int(input())
+int_c = int(input())
+
+if ((int_a + int_b) > int_c):
+    print(True)
+else:
+    print(False)
